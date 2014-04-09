b0a8185495f43e7f17aebbde5ef43225362ed6da
RobinCarozzani <contact@robincarozzani.fr>
2014-04-09 21:01:27 +0200

    Update Processing.java

--- src/vision/Processing.java
+++ src/vision/Processing.java
@@ -5,16 +5,23 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Vector;
 
+import org.opencv.core.Core;
 import org.opencv.core.Mat;
 import org.opencv.core.MatOfPoint;
 import org.opencv.core.MatOfPoint2f;
+import org.opencv.core.Point;
+import org.opencv.core.Rect;
 import org.opencv.core.Scalar;
 import org.opencv.imgproc.Imgproc;
 
+import com.atul.JavaOpenCV.Imshow;
+
 public class Processing {
 	
-	private static Mat img2;
+	private static Mat source, img2;
 	private static List<MatOfPoint> zones;
+	private static Imshow game = new Imshow("Jeu");
+	private static Imshow next = new Imshow("Suivants");
 	
 	private static Mat img2bw (Mat src) {
 		Mat img = new Mat();
@@ -23,49 +30,44 @@ public class Processing {
 		return img;
 	}
 	
-	private static int calculValeur (int lig, int col, int maxLig, int pasLig, int minCol, int maxCol, int pasCol, Mat img) {
-		double[] val = img.get(lig, col);
-		if ((int)val[0]/255 == 0) {
-			return 0;
-		}
-		boolean vide = false;
-		for (int i=lig ; i<maxLig ; i+=pasLig) {
-			val = img.get(i, col);
-			if ((int)val[0]/255 == 0) {
-				vide = true;
-				break;
+	public static void getNext () {
+		if (zones.size() > 2) {
+			MatOfPoint2f zone2 = new MatOfPoint2f();
+			Imgproc.approxPolyDP(new MatOfPoint2f(zones.get(2).toArray()), zone2, 4.0, true);
+			double[] coins = zone2.get(0, 0);
+			int minCol = (int)coins[0];
+			int maxCol = minCol;
+			int minLig = (int)coins[1];
+			int maxLig = minLig;
+			for (int i=1 ; i<zone2.rows() ; i++) {
+				coins = zone2.get(i, 0);
+				if ((int)coins[0] < minCol) {
+					minCol = (int)coins[0];
+				}
+				if ((int)coins[0] > maxCol) {
+					maxCol = (int)coins[0];
+				}
+				if ((int)coins[1] < minLig) {
+					minLig = (int)coins[1];
+				}
+				if ((int)coins[1] > maxLig) {
+					maxLig = (int)coins[1];
+				}
 			}
-		}
-		if (!vide) {
-			return 1;
-		}
-		val = img.get(lig, col);
-		vide = false;
-		for (int i=col ; i<maxCol ; i+=pasCol) {
-			val = img.get(lig, i);
-			if ((int)val[0]/255 == 0) {
-				vide = true;
-				break;
+			Rect roi = new Rect(minCol, minLig, maxCol-minCol, maxLig-minLig);
+			Mat src = new Mat(source.clone(), roi);
+			Mat img = new Mat(img2.clone(), roi);
+			List<MatOfPoint> contours = new Vector<MatOfPoint>();
+			Mat hierarchy = new Mat();
+			Imgproc.findContours(img, contours, hierarchy, 3, 2);
+			Collections.sort(contours, new ComparaisonContour());
+			for (int i=0 ; i<contours.size() ; i++) {
+				Imgproc.drawContours(src, contours, i, new Scalar(0, 255, 0), 2);
 			}
+			next.showImage(src);
 		}
-		if (!vide) {
-			return 1;
-		}
-		val = img.get(lig, col);
-		vide = false;
-		for (int i=col ; i>minCol ; i-=pasCol) {
-			val = img.get(lig, i);
-			if ((int)val[0]/255 == 0) {
-				vide = true;
-				break;
-			}
-		}
-		if (!vide) {
-			return 1;
-		}
-		return 2;
 	}
-	
+
 	public static List<List<Integer>> getJeu () {
 		List<List<Integer>> res = null;
 		if (zones.size() > 1) {
@@ -91,32 +93,60 @@ public class Processing {
 					maxLig = (int)coins[1];
 				}
 			}
-
+			Rect roi = new Rect(minCol, minLig, maxCol-minCol, maxLig-minLig);
+			Mat src = new Mat(source.clone(), roi);
+			Mat img = new Mat(img2.clone(), roi);
+			List<MatOfPoint> contours = new Vector<MatOfPoint>();
+			Mat hierarchy = new Mat();
+			Imgproc.findContours(img, contours, hierarchy, 3, 2);
+			Collections.sort(contours, new ComparaisonContour());
+			if (contours.size() > 2) {
+				MatOfPoint2f c = new MatOfPoint2f(contours.get(1).toArray());
+				double[] coord = c.get(1, 0);
+				Point p = new Point(coord[0], coord[1]);
+				while (Imgproc.pointPolygonTest(c, p, false) < 0 && contours.size() > 2) {
+					contours.remove(2);
+				}
+			}
+			for (int i=0 ; i<3 && i<contours.size() ; i++) {
+				Imgproc.drawContours(src, contours, i, new Scalar(0, 255, 0), 2);
+			}
 			int pasLig = (maxLig-minLig)/20;
 			int pasCol = (maxCol-minCol)/10;
-			if (pasLig > 0 && pasCol > 0) {
+			if (pasLig > 0 && pasCol > 0 && contours.size()>2) {
 				res = new ArrayList<List<Integer>>();
 				int k=0;
-				for (int i=minLig ; i<maxLig ; i+=pasLig) {
+				MatOfPoint2f c = new MatOfPoint2f(contours.get(1).toArray());
+				MatOfPoint2f c2 = new MatOfPoint2f(contours.get(2).toArray());
+				for (int i=pasLig/2 ; i<20*pasLig ; i+=pasLig) {
 					res.add(new ArrayList<Integer>());
-					for (int j=minCol ; j<maxCol ; j+=pasCol) {
-						int val = calculValeur(i, j, maxLig, pasLig, minCol, maxCol, pasCol, img2); 
-						res.get(k).add(val);
+					for (int j=pasCol/2 ; j<10*pasCol ; j+=pasCol) {
+						Point p = new Point((double)j, (double)i);
+						if (Imgproc.pointPolygonTest(c2, p, false) >= 0) {
+							res.get(k).add(2);
+							//Core.line(src, p, p, new Scalar(255, 101, 101));
+							Core.rectangle(src, new Point((double)j-pasCol/2, (double)i-pasLig/2), new Point((double)j+pasCol/2, (double)i+pasLig/2), new Scalar(0,0,255),-1);
+
+						} else if (Imgproc.pointPolygonTest(c, p, false) >= 0) {
+							res.get(k).add(0);
+							//Core.line(src, p, p, new Scalar(255, 255, 255));
+							Core.rectangle(src, new Point((double)j-pasCol/2, (double)i-pasLig/2), new Point((double)j+pasCol/2, (double)i+pasLig/2), new Scalar(255,255,255),-1);
+						} else {
+							res.get(k).add(1);
+							//Core.line(src, p, p, new Scalar(255, 0, 0));
+							Core.rectangle(src, new Point((double)j-pasCol/2, (double)i-pasLig/2), new Point((double)j+pasCol/2, (double)i+pasLig/2), new Scalar(255,0,0),-1);
+						}
 					}
 					k++;
 				}
 			}
-			if (res != null) {
-				res.remove(0);
-				for (int i=0 ; i<res.size() ; i++) {
-					res.get(i).remove(0);
-				}
-			}
+			game.showImage(src);
 		}
 		return res;
 	}
 
 	public static Mat imgContours (Mat src) {
+		source = src.clone();
 		Mat img = img2bw(src);
 		img2 = img.clone();
 		List<MatOfPoint> contours = new Vector<MatOfPoint>();
