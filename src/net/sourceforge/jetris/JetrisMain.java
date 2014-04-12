f672d5f6e2b6b8972187d7e8acb59caaeeafcd2b
Gaëtan Le Barbé <lebarbe@ecole.ensicaen.fr>
2014-04-12 11:53:59 +0200

    debut integration ia/vision

--- src/net/sourceforge/jetris/JetrisMain.java
+++ src/net/sourceforge/jetris/JetrisMain.java
@@ -1,6 +1,8 @@
 package net.sourceforge.jetris;
 
+import vision.Vision;
 import ai.IAMain;
+import ai.input.CameraInput;
 import ai.input.GameInput;
 
 public class JetrisMain
@@ -10,12 +12,14 @@ public class JetrisMain
 	public static void main(String[] args)
 	{
 		mf = new JetrisMainFrame();
+		Vision.show();
 		solve();
 	}
 	
 	public static void solve()
 	{
-		(new Thread(new IAMain(new GameInput(mf)))).start();
+		//(new Thread(new IAMain(new GameInput(mf)))).start();
+		(new Thread(new IAMain(new CameraInput()))).start();
 	}
 
 }
